e363fbfaf2f5256b63dcbc1c2a8c82ad24624f8f
Ashubr <95558162+JiayunZou@users.noreply.github.com>
2022-03-12 10:29:47 +0800

    Delete MYAPP directory

--- MYAPP/My_Menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MY_MENU_H_
-#define _MY_MENU_H_
-
-#include "common.h"
-
-bool my_menu_open(void);
-	
-#endif
--- MYAPP/altitude_display.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ALITITUDE_DISPLAY_H_
-#define _ALITITUDE_DISPLAY_H_
-
-#include "common.h"
-
-static display_t draw(void);
-bool altitude_open(void);
-void history_display(void);
-#endif
